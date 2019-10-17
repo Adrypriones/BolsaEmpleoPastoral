ba110332f2c0de05bacb450f2bcbec3897505e7a
Adriana <adrianamartineztrf@gmail.com>
2019-10-17 11:49:27 -0500

    wq wq git log Bolsa Empleo Pastoral

---
+++ PastoralEmpleo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PastoralEmpleo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
